d1e605f2d12d286ec47ce8bd61dcb532c244aa91
unknown <pablolope8@gmail.com>
2021-12-21 21:19:52 -0500

    Añadiendo variables para funcionamiento de node y mongo

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { server } from "./server";
 import "./database";
 
 server.start({ port: 4000 }, ({port}) => {
-    console.log('Servidor en puerto ', port)
+    console.log('Server in port ', port)
 })
